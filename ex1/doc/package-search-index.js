de89fc0c9674330f613a1542b4270bcf6f46d26f
Ivan <Ivan@Ivan321>
2020-11-26 23:00:28 +0200

    laste anotaition update

---
+++ ex1/doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"ex1"},{"l":"ex1.src"},{"l":"ex1.tests"}]
---
+++ ex1/doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ex1.src","l":"Edge"},{"p":"ex1.src","l":"node_info"},{"p":"ex1","l":"Structure"},{"p":"ex1.src","l":"weighted_graph"},{"p":"ex1.src","l":"weighted_graph_algorithms"},{"p":"ex1.src","l":"WGraph_Algo"},{"p":"ex1.src","l":"WGraph_DS"}]
